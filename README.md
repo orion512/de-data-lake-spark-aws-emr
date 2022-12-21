c3712c54751d099f8ccbe776c16aa01b7bbcf4de
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-12-21 00:07:44 +0000

    etl.py finalised. Added extracted data. Final step is to test on EMR cluster

--- README.md
+++ README.md
@@ -1,37 +1,14 @@
-# Data Modelling with AWS Data Warehouses (Redshift)
+# Data Lake with AWS (EMR Cluster)
 
-This repository serves as a submission for Udacity data engineer nanodegree.
+This repository serves as a submission for Udacity data engineering nanodegree.
 
 ## Project Introduction
-A music streaming startup, Sparkify, has grown their user base and song database and want to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+A music streaming startup, Sparkify, has grown their user base and song database even more and want to move their data warehouse to a data lake. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
 
 ## How to Run?
 
 This section describes how to get use this repositrory.
 
-**Redshift Cluster Setup**
-
-To run this project you will need connectivity to an AWS Redshift database.
-The setup_redshift.ipynb notebook will setup the cluster.
-In order for the notebook to run you will need a "dwh_udacity.cgf" file in 
-the same folder as the notebook, with below structure.
-```
-[AWS]
-KEY=
-SECRET=
-
-[DWH] 
-DWH_CLUSTER_TYPE=multi-node
-DWH_NUM_NODES=4
-DWH_NODE_TYPE=dc2.large
-
-DWH_IAM_ROLE_NAME=
-DWH_CLUSTER_IDENTIFIER=
-DWH_DB=
-DWH_DB_USER=dwhuser
-DWH_DB_PASSWORD=
-DWH_PORT=
-```
 
 **Python environemnt setup**
 ```
@@ -43,17 +20,7 @@ in the same folder as the .py files with the below structure.
 [CLUSTER]
 HOST=''
 DB_NAME=''
-DB_USER=''
-DB_PASSWORD=''
-DB_PORT=''
 
-[IAM_ROLE]
-ARN=
-
-[S3]
-LOG_DATA=''
-LOG_JSONPATH=''
-SONG_DATA=''
 ```
 
 **Initialize the database**
@@ -73,13 +40,3 @@ python etl.py
 \sql_queries.py --> contains SQL queries run throughout the project
 \setup_redshift.ipynb --> jupyter notebook to setup redshift, IAM roles.
 ```
-
-## Database design
-
-The goal of the project is to run efficient queries on song playing analytics which is mainly sotred in the songplays table.
-The design of the tables remains the same as before but the main difference
-now is taht the DB is deployed in a IAAS environemnt which is highly scalable.
-
-The ETL is integrated to read from AWS S3 buckets into staging table on the
-Redshift cluster. Then additional queries trasnform and move the data into
-their final tables to be used for analytics.
--- etl.py
+++ etl.py
@@ -10,8 +10,8 @@ from pyspark.sql.functions import year, month, hour, weekofyear, date_format, da
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -24,7 +24,7 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = os.path.join(input_data, 'song_data/*/*/*/*.json')
+    song_data = os.path.join(input_data, 'song-data/*/*/*/*.json')
     
     # read song data file
     df = spark.read.json(song_data)
@@ -50,7 +50,7 @@ def process_song_data(spark, input_data, output_data):
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data = os.path.join(input_data, 'log_data/*.json') #'log_data/*/*/*')
+    log_data = os.path.join(input_data, 'log-data/*.json') #'log_data/*/*/*')
 
     # read log data file
     df = spark.read.json(log_data)
@@ -86,7 +86,7 @@ def process_log_data(spark, input_data, output_data):
         'year', 'month').parquet(os.path.join(output_data, "time.parquet"))
 
     # read in song data to use for songplays table
-    song_data = os.path.join(input_data, 'song_data/*/*/*/*.json')
+    song_data = os.path.join(input_data, 'song-data/*/*/*/*.json')
     song_df = spark.read.json(song_data)
 
     # extract columns from joined song and log datasets to create songplays table 
@@ -112,12 +112,16 @@ def process_log_data(spark, input_data, output_data):
 
 
 def main():
+    print("Starting Script !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     spark = create_spark_session()
-    input_data = "data" # "s3a://udacity-dend/"
-    output_data = "output"
+    print("Spark Session Created !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    input_data ="s3a://udacity-dend/" # "data"
+    output_data = "s3a://output-udacity/" # "output"
     
-    process_song_data(spark, input_data, output_data)    
+    process_song_data(spark, input_data, output_data)  
+    print("Song Data Processed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     process_log_data(spark, input_data, output_data)
+    print("Spark Processing Finished !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 
 if __name__ == "__main__":
