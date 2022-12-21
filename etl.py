6bb80a7d9e35b8deea8a44b7b0f01256286fe7a9
Dominik ZS <34664183+orion512@users.noreply.github.com>
2022-12-21 09:50:53 +0000

    Applied required changes
    Added multiline doc-string comments and dropped duplicate rows.

--- etl.py
+++ etl.py
@@ -15,6 +15,9 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    """
+    This function creates a spark session
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -23,6 +26,10 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    This function processes song data by reading it from json files,
+    atransforming it into the desired format and writing it back to parquet files
+    """
     # get filepath to song data file
     song_data = os.path.join(input_data, 'song-data/*/*/*/*.json')
     
@@ -31,7 +38,7 @@ def process_song_data(spark, input_data, output_data):
 
     # extract columns to create songs table
     songs_table = df.select([
-        "song_id", "title", "artist_id", "year", "duration"])
+        "song_id", "title", "artist_id", "year", "duration"]).dropDuplicates()
     
     # write songs table to parquet files partitioned by year and artist
     songs_table.write.mode('overwrite').partitionBy(
@@ -41,7 +48,7 @@ def process_song_data(spark, input_data, output_data):
     # extract columns to create artists table
     artists_table = df.select([
         "artist_id", "artist_name", "artist_location", 
-        "artist_latitude", "artist_longitude"])
+        "artist_latitude", "artist_longitude"]).dropDuplicates()
     
     # write artists table to parquet files
     artists_table.write.mode('overwrite').parquet(
@@ -49,6 +56,10 @@ def process_song_data(spark, input_data, output_data):
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+    This function processes log data by reading it from json files,
+    atransforming it into the desired format and writing it back to parquet files
+    """
     # get filepath to log data file
     log_data = os.path.join(input_data, 'log-data/*/*')
 
@@ -60,7 +71,7 @@ def process_log_data(spark, input_data, output_data):
 
     # extract columns for users table    
     users_table = df.select([
-        'userId', 'firstName', 'lastName', 'gender', 'level'])
+        'userId', 'firstName', 'lastName', 'gender', 'level']).dropDuplicates()
     
     # write users table to parquet files
     users_table.write.mode('overwrite').parquet(
@@ -79,7 +90,7 @@ def process_log_data(spark, input_data, output_data):
         .withColumn('weekday', dayofweek(col("timestamp")))
     
     # extract columns to create time table
-    time_table = df.select('timestamp', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_table = df.select('timestamp', 'hour', 'day', 'week', 'month', 'year', 'weekday').dropDuplicates()
     
     # write time table to parquet files partitioned by year and month
     time_table.write.mode('overwrite').partitionBy(
@@ -105,7 +116,7 @@ def process_log_data(spark, input_data, output_data):
         .withColumnRenamed('timestamp', 'start_time') \
         .withColumnRenamed('userId', 'user_id') \
         .withColumnRenamed('sessionId', 'session_id') \
-        .withColumnRenamed('userAgent', 'user_agent')
+        .withColumnRenamed('userAgent', 'user_agent').dropDuplicates()
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table.write.mode('overwrite').partitionBy('year', 'month').parquet(os.path.join(output_data, "songplays.parquet"))
